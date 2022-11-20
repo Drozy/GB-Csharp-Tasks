a696fc7d2d18abc4ad3d34426aa2a573404313a1
Yakovlev Andrew <drozy@yandex.ru>
2022-11-20 13:20:22 +0300

    Add Task02

---
+++ Task02/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт,
+// какое сило большее, а какое меньшее.
+
+Console.Write("Введите первое число: ");
+int numOne = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int numTwo = Convert.ToInt32(Console.ReadLine());
+
+if (numOne > numTwo)
+{
+    Console.WriteLine($"Первое число {numOne} больше второго {numTwo}");
+}
+else if (numOne == numTwo) 
+    {
+        Console.WriteLine("Числа равны");
+    }
+    else Console.WriteLine($"Второе число {numTwo} больше первого {numOne}");
