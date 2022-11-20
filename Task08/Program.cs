435c44456ed7347f9e3b8367b72c2407a2c8b2d8
Yakovlev Andrew <drozy@yandex.ru>
2022-11-20 14:20:47 +0300

    Mod Task08 - output include N

--- Task08/Program.cs
+++ Task08/Program.cs
@@ -1,20 +1,19 @@
 ﻿// Напишите программу, которая на вход принимает число (N), 
 // а на выходе показывает все четные числа от 1 до N
-// Само N не включаю
 
 Console.Write("Введите число: "); 
 int N = Convert.ToInt32(Console.ReadLine());
 
 if (N > 0)  
 {
-    for (int i=2; i < N; i+=2)
+    for (int i=2; i <= N; i+=2)
     {
         Console.Write($"{i} ");
     }
 }
 else    // а вдруг ввели отрицательное число
 {
-    for (int i=0; i > N; i-=2)
+    for (int i=0; i >= N; i-=2)
     {
         Console.Write($"{i} ");
     }
