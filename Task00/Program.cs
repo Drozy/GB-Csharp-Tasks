b39c2879aff054d97ebc53a827997196eabda330
Yakovlev Andrew <drozy@yandex.ru>
2022-11-20 12:31:20 +0300

    Init commit

---
+++ Task00/Program.cs
@@ -0,0 +1,5 @@
+﻿// Принимаем число и выдаем его квадрат
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int result = num * num;
+Console.WriteLine($"Квадрат введенного числа: {result}");
---
+++ Task03/Program.cs
@@ -0,0 +1,33 @@
+﻿// 3. Напишите программу, которая будет выдавать название дня недели по заданному номеру.
+// 3 -> Среда 
+// 5 -> Пятница
+
+Console.Write("Введите номер дня: ");
+int numDay = Convert.ToInt32(Console.ReadLine());
+switch (numDay)
+{
+    case 1:
+        Console.WriteLine($"{numDay} -> понедельник");
+        break;
+    case 2:
+        Console.WriteLine($"{numDay} -> вторник");
+        break;
+    case 3:
+        Console.WriteLine($"{numDay} -> среда");
+        break;
+    case 4:
+        Console.WriteLine($"{numDay} -> четверг");
+        break;
+    case 5:
+        Console.WriteLine($"{numDay} -> пятница");
+        break;
+    case 6:
+        Console.WriteLine($"{numDay} -> суббота");
+        break;
+    case 7:
+        Console.WriteLine($"{numDay} -> воскресенье");
+        break;
+    default:
+        Console.WriteLine("Неправильный номер");
+        break;
+}
