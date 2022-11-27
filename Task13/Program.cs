0fa3269ebea5b602a3d8e22bd0962036dc001a7e
Yakovlev Andrew <drozy@yandex.ru>
2022-11-27 17:09:34 +0300

    Added 2nd homework

---
+++ Task13/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+// Пример:
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+double exp = 1 + Math.Truncate(Math.Log10(num)); // получили количество цифр
+
+if (exp < 3)
+    Console.WriteLine("Третьей цифры нет.");
+else
+{
+    double result = Math.Truncate(num % Math.Pow(10, exp-2) / Math.Pow(10, exp-3));
+    Console.WriteLine($"Третья цифра введенного числа: {result}");
+}
+
---
+++ Task15/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели,
+// и проверяет, является ли этот день выходным.
+
+Console.Write("Введите номер дня недели: ");
+int dayNum = Convert.ToInt32(Console.ReadLine());
+if ((dayNum < 1) || (dayNum > 7))
+    Console.WriteLine("ошибка ввода");
+else if (dayNum < 6)
+    Console.WriteLine("Рабочий день");
+else Console.WriteLine("Выходной");
