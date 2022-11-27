96925f44fe5771cd724dd8107de8cb2d2374fb96
Yakovlev Andrew <drozy@yandex.ru>
2022-11-27 16:58:49 +0300

    Added tasks from 2nd and 3rd lessons

---
+++ Task2_3/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая будет принимать на вход два числа и выводить, является ли второе число кратным первому. Если число 2 не кратно числу 1, 
+// то программа выводит остаток от деления.
+
+// 34, 5 -> не кратно, остаток 4 
+// 16, 4 -> кратно
+
+Console.Write("Введите первое число: ");
+int numOne = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int numTwo = Convert.ToInt32(Console.ReadLine());
+
+int result = numOne % numTwo;
+// Console.WriteLine(result);
+if (result == 0) 
+{
+    Console.WriteLine("Кратно");
+}
+else 
+{
+    Console.WriteLine($"Не кратно, остаток {result}");
+}
---
+++ Task3_1/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача №17. Напишите программу, которая принимает на вход координаты точки (X и Y), 
+// причем X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка.
+
+Console.WriteLine("Задайте координаты точки");
+Console.Write("Введите X: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите Y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+if ((x == 0) || (y == 0)) 
+Console.WriteLine("Координаты не должны быть равны 0");
+else
+{
+    if (x > 0)
+    {
+         if (y > 0) Console.WriteLine("1 четверть");
+         else Console.WriteLine("4 четверть");
+    }
+    else 
+    {
+        if (y > 0) Console.WriteLine("2 четверть");
+        else Console.WriteLine("3 четверть");
+    }
+}
