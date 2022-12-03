a24d047609c1203209b12698ebd8a6c865e06e1f
Yakovlev Andrew <drozy@yandex.ru>
2022-12-03 18:14:55 +0300

    Task 27 modified

--- Task27/Program.cs
+++ Task27/Program.cs
@@ -3,20 +3,14 @@
 // 82 -> 10
 // 9012 -> 12
 
-int sumDigit(int num)
+int sumDigit(int n)
 {
     int result = 0;
-    num = Math.Abs(num);
-    if (num < 10)
-        result = num;
-    else
+    n = Math.Abs(n);
+    while(n > 0)
     {
-        int k = num;
-        while(k > 0)
-        {
-            result = result + k%10;
-            k = k/10;
-        }
+        result = result + n%10;
+        n = n/10;
     }
     return result;
 }
