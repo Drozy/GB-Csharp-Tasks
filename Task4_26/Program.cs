fb67505c5c7ac89ca092afdeca17dc03c68dc69c
Yakovlev Andrew <drozy@yandex.ru>
2022-12-03 11:52:00 +0300

    Added task from Lesson4

---
+++ Task4_26/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача 26: Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+Console.Write("Введите число: ");
+double num = Convert.ToDouble(Console.ReadLine());
+double exp = 1 + Math.Truncate(Math.Log10(num)); // получили количество цифр
+
+Console.WriteLine($"Количество цифр {exp}");
