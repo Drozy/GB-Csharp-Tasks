d3f0e1702abee6477fa5be4569f850b0b184709d
Yakovlev Andrew <drozy@yandex.ru>
2022-12-04 12:16:50 +0300

    Added tasks from Lesson5 (whithout 37)

---
+++ Task5_31/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 31: Задайте массив из 12 элементов, заполненный случайными числами из промежутка [-9, 9]. Найдите сумму отрицательных и положительных элементов массива.
+// Например, в массиве [3,9,-8,1,0,-7,2,-1,8,-3,-1,6] сумма положительных чисел равна 29, сумма отрицательных равна -20.
+
+int[] arrRand(int len)      // функция заполнения массива произвольной длины случайными числами
+{
+    int[] a = new int[len];
+    for (int i=0; i < a.Length; i++)
+    {
+        a[i] = new Random().Next(-9,10); // диапазон можно менять
+    }
+    return a;
+}
+
+void arrPrint(int[] a)      // функция вывода массива
+{
+    for (int i=0; i < a.Length; i++)
+        Console.Write($"{a[i]} ");
+    Console.WriteLine();
+}
+
+
+int[] arr = arrRand(12);
+arrPrint(arr);
+int sumPlus = 0;
+int sumMin = 0;
+
+foreach (var n in arr)
+{
+    if (n > 0) sumPlus = sumPlus + n;
+    else sumMin = sumMin + n;
+}
+
+Console.WriteLine($"Сумма положительных элементов равна {sumPlus}");
+Console.WriteLine($"Сумма отрицателных элементов равна {sumMin}");
---
+++ Task5_33/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 33: Задайте массив. Напишите программу, которая определяет, присутствует ли заданное число в массиве.
+// 4; массив [6, 7, 19, 345, 3] -> нет
+// -3; массив [6, 7, 19, 345, 3] -> да
+
+int[] arrRand(int len)      // функция заполнения массива произвольной длины случайными числами
+{
+    int[] a = new int[len];
+    for (int i=0; i < a.Length; i++)
+    {
+        a[i] = new Random().Next(0,100); // диапазон можно менять
+    }
+    return a;
+}
+
+void arrPrint(int[] a)      // функция вывода массива
+{
+    for (int i=0; i < a.Length; i++)
+        Console.Write($"{a[i]} ");
+    Console.WriteLine();
+}
+
+int[] arr = arrRand(10);
+arrPrint(arr);
+
+Console.Write("Введите искомое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+bool result = false;
+foreach (var a in arr)
+{
+    if (a == num) 
+    {
+        result = true;
+        break;
+    }
+}
+
+if (result) Console.WriteLine($"Число {num} есть в массиве");
+else Console.WriteLine($"Числа {num} нет в массиве");
+
+
