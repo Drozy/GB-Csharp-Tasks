1c5806d10d13acc774a4bd9b589a2ca300f21491
Yakovlev Andrew <drozy@yandex.ru>
2022-12-18 12:07:07 +0300

    Added tasks from Lesson 9

---
+++ Task9_63/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 63: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
+// N = 5 -> "1, 2, 3, 4, 5"
+// N = 6 -> "1, 2, 3, 4, 5, 6"
+
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+string nums = string.Empty;
+
+// не самый лучший вывод через строковую переменную
+void Enumeration(int a)
+{
+    if (a == 1) nums = "1 " + nums;
+    else
+    {
+        nums = Convert.ToString(a) + " " + nums;
+        Enumeration(a - 1);
+    }
+}
+Enumeration(n);
+Console.WriteLine(nums);
