cc44248d8ae79589f65fdf986154e5be32b9f2d9
Yakovlev Andrew <drozy@yandex.ru>
2022-12-18 21:49:54 +0300

    8th homework in progress

---
+++ Task56/Program.cs
@@ -0,0 +1,67 @@
+﻿// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+Console.Write("Введите количество строк массива: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] arr = FillMatrixRnd(m, n);
+PrintMatrix(arr);
+Console.WriteLine($"Строка с наименьшей суммой элементов: {IndexMinRow(arr) + 1}"); // +1 для счета с 1
+
+// Заполнение массива случайными числами
+int[,] FillMatrixRnd(int row, int col)
+{
+    int[,] matrix = new int[row, col];
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            matrix[i, j] = new Random().Next(1, 10);
+        }
+    }
+    return matrix;
+}
+
+// Вывод двумерного массива
+void PrintMatrix(int[,] matrix)
+{
+    Console.WriteLine();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+// Нахождение индекса строки с наименьшей суммой элементов
+int IndexMinRow(int[,] matrix)
+{
+    int[] sumRows = new int[matrix.GetLength(0)];
+    for (int i = 0; i < matrix.GetLength(0); i++)   // сохраняем суммы элементов каждой строки в одномерный массив
+    {
+        sumRows[i] = 0;
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            sumRows[i] += matrix[i, j];
+        }
+    }
+    int min = sumRows[0];
+    int minIndex = 0;
+    for (int i = 1; i < matrix.GetLength(0); i++)   // находим наименьшую сумму и её индекс
+        if (min > sumRows[i])
+        {
+            min = sumRows[i];
+            minIndex = i;
+        }
+    return minIndex;
+}
---
+++ Task58/Program.cs
@@ -0,0 +1,67 @@
+﻿// Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+// Например, даны 2 матрицы:
+// 2 4 | 3 4
+// 3 2 | 3 3
+// Результирующая матрица будет:
+// 18 20
+// 15 18
+
+// Будем задавать квадратные матрицы, чтобы не проверять их на совместимость/согласованность.
+const int size = 3; // размер матриц
+int[,] arr1 = CreateMatrix();
+int[,] arr2 = CreateMatrix();
+int[,] mult = MatrixMultiplication(arr1, arr2);
+
+Console.WriteLine("\nМатрица A");
+PrintMatrix(arr1);
+Console.WriteLine("Матрица B");
+PrintMatrix(arr2);
+Console.WriteLine("Произведение матриц АB");
+PrintMatrix(mult);
+
+// Заполнение массива случайными числами
+int[,] CreateMatrix()
+{
+    int[,] matrix = new int[size, size];
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < size; j++)
+        {
+            matrix[i, j] = new Random().Next(1, 10);
+        }
+    }
+    return matrix;
+}
+
+// Вывод матрицы
+void PrintMatrix(int[,] matrix)
+{
+    Console.WriteLine();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+// Произведение матриц AB состоит из всех возможных комбинаций скалярных произведений вектор-строк матрицы A и вектор-столбцов матрицы B.
+int[,] MatrixMultiplication(int[,] a, int[,] b)
+{
+    int[,] ab = new int[a.GetLength(0), b.GetLength(1)];    // такие размеры, чтобы функция принимала любые матрицы
+    for (int i = 0; i < ab.GetLength(0); i++)
+    {
+        for (int j = 0; j < ab.GetLength(1); j++)
+        {
+            ab[i, j] = 0;
+            for (int k = 0; k < a.GetLength(1); k++)
+            {
+                ab[i, j] += a[i, k] * b[k, j];
+            }
+        }
+    }
+    return ab;
+}
