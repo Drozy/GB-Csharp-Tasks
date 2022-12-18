3e85450f80cbdce7c9f1680a6053634994f44bc7
Yakovlev Andrew <drozy@yandex.ru>
2022-12-18 19:39:26 +0300

    9th homework

---
+++ Task66/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+Console.Write("Введите число A: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число B: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+double SegmentSum(int a, int b)
+{
+    if (a == b || a + 1 == b || a == b + 1) return 0;   // нет элементов в промежутке
+    else if (a > b) return b + 1 + SegmentSum(a, b + 1);
+    else return a + 1 + SegmentSum(a + 1, b);
+}
+
+Console.WriteLine($"Сумма натуральных элементов в промежутке от {m} до {n} равна {SegmentSum(m, n)}.");
---
+++ Task68/Program.cs
@@ -0,0 +1,33 @@
+﻿// Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+
+// Из Wiki:
+// функция ack(n, m)
+//    пока n ≠ 0
+//      если m = 0
+//        m:= 1
+//      иначе
+//        m:= ack(n, m - 1)
+//      n:= n - 1
+//    вернуть m + 1
+
+double ack(double m, double n)
+{
+    while (n != 0)
+    {
+        if (m == 0) m = 1;
+        else m = ack(n, m - 1);
+        n = n - 1;
+    }
+    return m + 1;
+}
+
+// int ackM = 1;
+// int ackN = 1;
+// Console.WriteLine($"ack({ackM}, {ackN}) = {ack(ackM, ackN)}");
+
+for (int i = 0; i < 4; i++) // переполнение при расчете ack(3,1))
+{
+    Console.WriteLine();
+    for (int j = 0; j < 2; j++) // при j = 3 переполнение ~19k повторов :)
+        Console.Write($"ack({i}, {j}) = {ack(i, j)}\t");
+}
